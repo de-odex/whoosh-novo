8d5072c0ac89e675a02012d34cd7316d9831b747
de-odex <de-odex@users.noreply.github.com>
2025-03-15 20:24:29 +0900

    remove deprecated setuptools.command.test
    please directly use `nox` instead to run tests

--- setup.py
+++ setup.py
@@ -4,31 +4,10 @@ import os.path
 import sys
 
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-
-try:
-    import pytest
-except ImportError:
-    pytest = None
 
 sys.path.insert(0, os.path.abspath("src"))
 from whoosh import versionstring
 
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        # import here, cause outside the eggs aren't loaded
-        import pytest
-
-        pytest.main(self.test_args)
-
-
 setup(
     version=versionstring(),
-    cmdclass={"test": PyTest},
 )

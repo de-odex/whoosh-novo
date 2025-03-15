fed257b968014cf9b2296ec34f7f72102151a889
de-odex <de-odex@users.noreply.github.com>
2025-03-15 21:00:27 +0900

    remove nose from test dependencies
    stress tests are still broken in this state

--- stress/test_hugeindex.py
+++ stress/test_hugeindex.py
@@ -1,6 +1,5 @@
 import struct
 
-from nose.tools import assert_equal  # type: ignore @UnresolvedImport
 from whoosh import formats
 from whoosh.filedb.filepostings import FilePostingReader, FilePostingWriter
 from whoosh.util.testing import TempStorage
@@ -13,7 +12,7 @@ def test_huge_postfile():
         gb5 = 5 * 1024 * 1024 * 1024
         pf.seek(gb5)
         pf.write("\x00\x00\x00\x00")
-        assert_equal(pf.tell(), gb5 + 4)
+        assert pf.tell() == gb5 + 4
 
         fpw = FilePostingWriter(pf)
         f = formats.Frequency(None)
@@ -21,16 +20,16 @@ def test_huge_postfile():
         for i in range(10):
             fpw.write(i, float(i), struct.pack("!I", i), 10)
         posttotal = fpw.finish()
-        assert_equal(posttotal, 10)
+        assert posttotal == 10
         fpw.close()
 
         pf = st.open_file("test.pst")
         pfr = FilePostingReader(pf, offset, f)
         i = 0
         while pfr.is_active():
-            assert_equal(pfr.id(), i)
-            assert_equal(pfr.weight(), float(i))
-            assert_equal(pfr.value(), struct.pack("!I", i))
+            assert pfr.id() == i
+            assert pfr.weight() == float(i)
+            assert pfr.value() == struct.pack("!I", i)
             pfr.next()
             i += 1
         pf.close()

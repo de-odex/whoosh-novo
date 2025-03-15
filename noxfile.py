02000c5ff2224a6f4986fa330f2c57dfb3be97c7
de-odex <de-odex@users.noreply.github.com>
2025-03-15 19:05:53 +0900

    replace tox with nox
    i was having trouble using tox the way i wanted to

---
+++ noxfile.py
@@ -0,0 +1,45 @@
+from nox import Session, options, project, session
+
+options.default_venv_backend = "uv"
+options.reuse_venv = "yes"
+options.sessions = ["tests"]
+
+PYPROJECT = project.load_toml("pyproject.toml")
+PYTHON_VERSIONS = project.python_versions(PYPROJECT)
+
+
+@session(python=PYTHON_VERSIONS)
+def test(session: Session):
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=test",
+        "--no-dev",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+
+    if session.posargs:
+        test_files = session.posargs
+    else:
+        test_files = ["tests"]
+
+    session.run("pytest", *test_files)
+
+
+@session(python=PYTHON_VERSIONS)
+def cov(session: Session):
+    session.run_install(
+        "uv",
+        "sync",
+        "--group=test",
+        "--group=cov",
+        "--no-dev",
+        env={"UV_PROJECT_ENVIRONMENT": session.virtualenv.location},
+    )
+
+    if session.posargs:
+        test_files = session.posargs
+    else:
+        test_files = ["tests"]
+
+    session.run("pytest", "--cov-branch", "--cov-report=xml", *test_files)
